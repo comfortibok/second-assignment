985f75b8c0bdeb53bd7a9e3d914742beaebb6b70
comfortibok <ibokcomfort93@gmail.com>
2024-11-25 21:37:52 +0100

    Updated assignment 6

---
+++ assignment5/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navList = document.querySelector(".nav-list");
+
+  menuIcon.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+});
