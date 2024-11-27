be21a3fca1d5ddef0122f1b2c92410a0aaaac657
comfortibok <ibokcomfort93@gmail.com>
2024-11-27 19:03:55 +0100

    Updated

--- assignment6/index.js
+++ assignment6/index.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.querySelector(".menu-icon");
   const navContainerList = document.querySelector(".nav-container-list");
-  const loginContainer = document.querySelector(".login-container");
-
+  
   menuIcon.addEventListener("click", () => {
     navContainerList.classList.toggle("active");
-    loginContainer.classList.toggle("active");
   });
 });
